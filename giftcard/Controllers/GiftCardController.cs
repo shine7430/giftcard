6a4d7bb003f9e9af5abc604d45df42152277266f
shine7430 <chaohui_shen@163.com>
2018-05-23 13:31:58 +0800

    cardtake

---
+++ giftcard/Controllers/GiftCardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllTrustUs.giftcard.Controllers
+{
+    public class GiftCardController : Controller
+    {
+        // GET: GiftCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
